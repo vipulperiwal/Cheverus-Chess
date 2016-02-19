d492a0d95d561aec887b82e0255899654da541dd
vipulperiwal <vipulperiwal@Vipul.local>
2016-02-19 10:27:54 -0500

    test
    upload code

---
+++ Cheverus Chess/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Cheverus Chess
+//
+//  Created by Vipul Periwal on 2/14/16.
+//  Copyright © 2016 Vipul Periwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+
+    override func viewDidLoad() {
+        UIApplication.sharedApplication().statusBarStyle = .LightContent;
+        emailField.text = "";
+        emailField.attributedPlaceholder = NSAttributedString(string:"Cheverus Email",
+            attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()]);
+        passwordField.text = "";
+        passwordField.attributedPlaceholder = NSAttributedString(string:"Password",
+            attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        
+        
+        
+        
+    }
+
+
+
+
+}
+
---
+++ Cheverus ChessTests/Cheverus_ChessTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cheverus_ChessTests.swift
+//  Cheverus ChessTests
+//
+//  Created by Vipul Periwal on 2/14/16.
+//  Copyright © 2016 Vipul Periwal. All rights reserved.
+//
+
+import XCTest
+@testable import Cheverus_Chess
+
+class Cheverus_ChessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
